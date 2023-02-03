b12a52eb07fa587329603964b57fe708835ba06b
puni9869 <punitinani1@hotmail.com>
2023-02-04 00:04:21 +0530

    Adding viteStaticCopy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { fileURLToPath, URL } from "node:url";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./CNAME",
+          dest: "."
+        }
+      ]
+    })
+  ],
   build: {
     outDir: "./docs"
   },
