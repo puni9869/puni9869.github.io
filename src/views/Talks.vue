de135298daaad4826477e60ff95560eea18b0144
puni9869 <punitinani1@hotmail.com>
2023-02-13 14:01:40 +0530

    Added home button

--- src/views/Talks.vue
+++ src/views/Talks.vue
@@ -8,9 +8,15 @@ import metadata from "@/assets/metadata.json";
       <div class="my-5 container">
         <div class="row my-5">
           <div class="col" v-if="metadata">
-            <h3>Talks</h3>
+            <p>
+              <router-link to="/">
+                <button class="btn btn-sm"><i class="fa fa-home"></i></button>
+              </router-link>
+              Talks
+            </p>
             <div class="row" v-for="talk in metadata" :key="talk.id">
-              <p class="title"><a :href="talk.url" target="_blank">// {{ talk.title }}</a> <span>{{ talk.published }}</span>
+              <p class="title">
+                // <a :href="talk.url" target="_blank">{{ talk.title }}</a> <span> {{ talk.published }}</span>
               </p>
             </div>
           </div>
@@ -20,3 +26,22 @@ import metadata from "@/assets/metadata.json";
     </div>
   </div>
 </template>
+
+<style scoped>
+.btn {
+  background-color: cadetblue;
+  border-radius: 0px;
+  color: white;
+  padding: 0 5px 0 5px;
+  font-size: 15px;
+  margin-top: 0px;
+  cursor: pointer;
+  margin-right: 5px;
+  margin-left: 5px;
+}
+
+.btn:hover {
+  background-color: black;
+  color: white
+}
+</style>
